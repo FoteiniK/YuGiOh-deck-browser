cefd6ceaa40b5e4750ac0a0e32b0bd6295e393bf
Foteini Kollia <Foteinikollia@gmail.com>
2018-11-05 09:03:23 +0200

    Add Image component

--- app/components/CardImage.js
+++ app/components/CardImage.js
@@ -1,15 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class CardImage extends Component {
-  state = {};
+const CardImage = props => {
+  let url = `http://52.57.88.137/api/card_image/${props.selectedCard}`;
+  return (
+    <div className="card-image">
+      {props.selectedCard && <img src={url} alt={props.selectedCard} />}
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className="card-image">
-        <img alt={this.props.selectedCard} />
-      </div>
-    );
-  }
-}
-
+CardImage.propTypes = {
+  selectedCard: PropTypes.string.isRequired
+};
 export default CardImage;
